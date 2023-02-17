0db4715cb09f86a3a85c2fd3819aec78db308816
Jon Gunderson <jongund@illinois.edu>
2023-02-17 10:10:23 -0600

    updated documentation

--- dist/skipto.js
+++ dist/skipto.js
@@ -1,4 +1,5 @@
 /* ========================================================================
+ * Version: 5.1.3
  * Copyright (c) 2022, 2023 Jon Gunderson; Licensed BSD
  * Copyright (c) 2021 PayPal Accessibility Team and University of Illinois; Licensed BSD
  * All rights reserved.
