76ceb4bdbc6005cdbd55c29615ff14f35c8956e8
Jon Gunderson <jongund@illinois.edu>
2024-08-22 09:21:20 -0700

    updating options

--- src/chrome/options.js
+++ src/chrome/options.js
@@ -1,6 +1,6 @@
 /* options.js */
 
-const debug = false;
+const debug = true;
 
 import {
   getOptions,
@@ -18,121 +18,132 @@ function notLastError () {
   }
 }
 
-function initForm () {
-  const landmarksNavInput = document.getElementById('landmarks-nav');
-  const landmarksSearchInput = document.getElementById('landmarks-search');
-  const landmarksComplementaryInput = document.getElementById('landmarks-complementary');
-  const landmarksContentinfoInput = document.getElementById('landmarks-contentinfo');
-  const landmarksBannerInput = document.getElementById('landmarks-banner');
+class OptionsContent {
 
-  const headings2Input = document.getElementById('headings-2');
-  const headings3Input = document.getElementById('headings-3');
-  const headings4Input = document.getElementById('headings-4');
-  const headings5Input = document.getElementById('headings-5');
-  const headings6Input = document.getElementById('headings-6');
-  const headingsMainOnlyInput = document.getElementById('headings-main-only');
+  constructor() {
 
-  getOptions().then( (options) => {
-    landmarksNavInput.checked = options.landmarks.includes('nav');
-    landmarksSearchInput.checked = options.landmarks.includes('search');
-    landmarksComplementaryInput.checked = options.landmarks.includes('complementary');
-    landmarksContentinfoInput.checked = options.landmarks.includes('contentinfo');
-    landmarksBannerInput.checked = options.landmarks.includes('banner');
+    const form = {};
 
-    headings2Input.checked = options.headings.includes('h2');
-    headings3Input.checked = options.headings.includes('h3');
-    headings4Input.checked = options.headings.includes('h4');
-    headings5Input.checked = options.headings.includes('h5');
-    headings6Input.checked = options.headings.includes('h6');
+    form.landmarksNavInput = document.getElementById('landmarks-nav');
+    form.landmarksSearchInput = document.getElementById('landmarks-search');
+    form.landmarksComplementaryInput = document.getElementById('landmarks-complementary');
+    form.landmarksContentinfoInput = document.getElementById('landmarks-contentinfo');
+    form.landmarksBannerInput = document.getElementById('landmarks-banner');
 
-    headingsMainOnlyInput.checked = options.headings.includes('main-only');
+    form.headings2Input = document.getElementById('headings-2');
+    form.headings3Input = document.getElementById('headings-3');
+    form.headings4Input = document.getElementById('headings-4');
+    form.headings5Input = document.getElementById('headings-5');
+    form.headings6Input = document.getElementById('headings-6');
+    form.headingsMainOnlyInput = document.getElementById('headings-main-only');
 
-  });
-}
+    this.form = form;
+  }
 
-async function sendOptionsToTabs (options) {
-  const tabs = await chrome.tabs.query({});
-  for (const tab of tabs) {
-    // Note: sensitive tab properties such as tab.title or tab.url can only be accessed for
-    // URLs in the host_permissions section of manifest.json
-      chrome.tabs.sendMessage(tab.id, {skiptoParams: optionsToParams(options)})
-      .then((response) => {
-          debug && console.info("Options received response from tab with title '%s' and url %s",
-              response.title, response.url)
-      })
-      .catch((error) => {
-              debug && console.warn("Options could not send message to tab %d", tab.id, error)
+  updateOptions () {
+    const form = this.form;
+
+    getOptions().then( (options) => {
+      form.landmarksNavInput.checked = options.landmarks.includes('nav');
+      form.landmarksSearchInput.checked = options.landmarks.includes('search');
+      form.landmarksComplementaryInput.checked = options.landmarks.includes('complementary');
+      form.landmarksContentinfoInput.checked = options.landmarks.includes('contentinfo');
+      form.landmarksBannerInput.checked = options.landmarks.includes('banner');
+
+      form.headings2Input.checked = options.headings.includes('h2');
+      form.headings3Input.checked = options.headings.includes('h3');
+      form.headings4Input.checked = options.headings.includes('h4');
+      form.headings5Input.checked = options.headings.includes('h5');
+      form.headings6Input.checked = options.headings.includes('h6');
+
+      form.headingsMainOnlyInput.checked = options.headings.includes('main-only');
+
+      this.syncOptionsWithSkipToScript (options);
+    });
+  }
+
+  syncOptionsWithSkipToScript (options) {
+    async function sendOptionsToTabs (options) {
+      debug && console.log(`[syncOptoinsWithSkipToScript]: ${options}`);
+      const tabs = await chrome.tabs.query({});
+      for (const tab of tabs) {
+          chrome.tabs.sendMessage(tab.id, {skiptoParams: optionsToParams(options)})
+          .then((response) => {
+              debug && console.info("Options received response from tab with title '%s' and url %s",
+                  response.title, response.url)
           })
+          .catch((error) => {
+                  debug && console.warn("Options could not send message to tab %d", tab.id, error)
+              })
+      }
+    }
+
+    sendOptionsToTabs(options);
+  }
+
+  saveOptions () {
+
+    const form = this.form;
+
+    getOptions().then( (options) => {
+
+      options.landmarks = 'main';
+
+      if (form.landmarksNavInput.checked) {
+        options.landmarks += ' nav';
+      }
+
+      if (form.landmarksSearchInput.checked) {
+        options.landmarks += ' search';
+      }
+
+      if (form.landmarksComplementaryInput.checked) {
+        options.landmarks += ' complementary';
+      }
+
+      if (form.landmarksContentinfoInput.checked) {
+        options.landmarks += ' contentinfo';
+      }
+
+      if (form.landmarksBannerInput.checked) {
+        options.landmarks += ' banner';
+      }
+
+      options.headings = form.headingsMainOnlyInput.checked ? "main-only h1" : "h1";
+
+      if (form.headings2Input.checked) {
+        options.headings += " h2";
+      }
+
+      if (form.headings3Input.checked) {
+        options.headings += " h2 h3";
+      }
+
+      if (form.headings4Input.checked) {
+        options.headings += " h2 h3 h4";
+      }
+
+      if (form.headings5Input.checked) {
+        options.headings += " h2 h3 h4 h5";
+      }
+
+      if (form.headings6Input.checked) {
+        options.headings += " h2 h3 h4 h5 h6";
+      }
+
+      saveOptions(options).then(sendOptionsToTabs(options));
+    });
   }
 }
 
-function saveForm () {
+document.addEventListener('DOMContentLoaded', () => {
+  const optionsContent = new OptionsContent();
+  optionsContent.updateOptions();
 
-  getOptions().then( (options) => {
-
-    const landmarksNavInput = document.getElementById('landmarks-nav');
-    const landmarksSearchInput = document.getElementById('landmarks-search');
-    const landmarksComplementaryInput = document.getElementById('landmarks-complementary');
-    const landmarksContentinfoInput = document.getElementById('landmarks-contentinfo');
-    const landmarksBannerInput = document.getElementById('landmarks-banner');
-
-    const headings2Input = document.getElementById('headings-2');
-    const headings3Input = document.getElementById('headings-3');
-    const headings4Input = document.getElementById('headings-4');
-    const headings5Input = document.getElementById('headings-5');
-    const headings6Input = document.getElementById('headings-6');
-    const headingsMainOnlyInput = document.getElementById('headings-main-only');
-
-    options.landmarks = 'main';
-
-    if (landmarksNavInput.checked) {
-      options.landmarks += ' nav';
-    }
-
-    if (landmarksSearchInput.checked) {
-      options.landmarks += ' search';
-    }
-
-    if (landmarksComplementaryInput.checked) {
-      options.landmarks += ' complementary';
-    }
-
-    if (landmarksContentinfoInput.checked) {
-      options.landmarks += ' contentinfo';
-    }
-
-    if (landmarksBannerInput.checked) {
-      options.landmarks += ' banner';
-    }
-
-    options.headings = headingsMainOnlyInput.checked ? "main-only h1" : "h1";
-
-    if (headings2Input.checked) {
-      options.headings += " h2";
-    }
-
-    if (headings3Input.checked) {
-      options.headings += " h2 h3";
-    }
-
-    if (headings4Input.checked) {
-      options.headings += " h2 h3 h4";
-    }
-
-    if (headings5Input.checked) {
-      options.headings += " h2 h3 h4 h5";
-    }
-
-    if (headings6Input.checked) {
-      options.headings += " h2 h3 h4 h5 h6";
-    }
-
-    saveOptions(options).then(sendOptionsToTabs(options));
+  document.getElementById('id-save').addEventListener('click', optionsContent.saveOptions.bind(optionsContent));
+  document.getElementById('id-reset-defaults').addEventListener('click', () => {
+    resetDefaultOptions().then(optionsContent.updateOptions.bind(optionsContent));
   });
-}
 
-document.addEventListener('DOMContentLoaded', initForm);
-document.getElementById('id-save').addEventListener('click', saveForm);
-document.getElementById('id-reset-defaults').addEventListener('click', () => {
-  resetDefaultOptions().then(initForm);
 });
+
