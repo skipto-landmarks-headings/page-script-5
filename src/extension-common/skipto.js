2849ca18bca9b5964058f6e59a1cc12e899e3ca9
Jon Gunderson <jongund@illinois.edu>
2024-10-08 12:42:52 -0500

    updated verison bug

--- src/extension-common/skipto.js
+++ src/extension-common/skipto.js
@@ -1,5 +1,5 @@
 /* ========================================================================
- * Version: 5.6.1
+ * Version: 5.6.2
  * Copyright (c) 2022, 2023, 2024 Jon Gunderson; Licensed BSD
  * Copyright (c) 2021 PayPal Accessibility Team and University of Illinois; Licensed BSD
  * All rights reserved.
