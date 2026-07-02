0e82785da81f937612556ad0dcd6252f0bf77b75
Jon Gunderson <jongund@illinois.edu>
2026-07-02 12:49:35 -0500

    updated extension build

--- build-extensions.sh
+++ build-extensions.sh
@@ -3,8 +3,12 @@ echo "Building extensions.."
 
 version="1.4.0"
 
-zip -r ./docs/dist/skipto-for-firefox-$version.zip extension-firefox/*  -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/skipto-for-chrome-$version.zip  extension-chrome/*   -x ".*" -x "__MACOSX"
-zip -r ./docs/dist/skipto-for-edge-$version.zip    extension-edge/*     -x ".*" -x "__MACOSX"
-crx3 extension-opera -p ../pem/opera-skipto.pem -o ./docs/dist/skipto-for-opera-$version.crx
+cd extension-firefox
+zip -r ../docs/dist/skipto-for-firefox-$version.zip *  -x ".*" -x "__MACOSX"
+cd ../extension-chrome
+zip -r ../docs/dist/skipto-for-chrome-$version.zip  *   -x ".*" -x "__MACOSX"
+cd ../extension-edge
+zip -r ../docs/dist/skipto-for-edge-$version.zip    *     -x ".*" -x "__MACOSX"
+cd ../extension-opera
+crx3 . -p ../../pem/opera-skipto.pem -o ../docs/dist/skipto-for-opera-$version.crx
 
