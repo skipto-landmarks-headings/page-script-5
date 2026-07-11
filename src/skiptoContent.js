44417407d724f1575a6a22785ad4570e33e69bed
Jon Gunderson <jongund@illinois.edu>
2026-07-10 19:33:41 -0500

    fixed create element name

--- src/skiptoContent.js
+++ src/skiptoContent.js
@@ -22,11 +22,11 @@ import {
 const debug = new DebugLogging('skiptoContent', false);
 debug.flag = false;
 
-/* @class SkipToContent5110
+/* @class SkipToContent5111
  *
  */
 
-export default class SkipToContent5110 extends HTMLElement {
+export default class SkipToContent5111 extends HTMLElement {
 
   constructor() {
     // Always call super first in constructor
